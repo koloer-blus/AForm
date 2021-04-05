50d2969131b30283b8e860d0fdf6440cdae8e8d3
baiziyu-fe <77950278+baiziyu-fe@users.noreply.github.com>
2021-04-05 16:33:55 +0800

    feat: 修改布局

--- src/pages/base-layout/index.tsx
+++ src/pages/base-layout/index.tsx
@@ -1,22 +1,33 @@
 import React from 'react';
-import { Layout } from 'antd';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Layout, Menu, Typography } from 'antd';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 import { routes } from './router';
 import { RenderRoutes } from '@/router/RenderRoutes';
 import './style.less';
+const { SubMenu } = Menu;
 const { Header, Footer, Sider, Content } = Layout;
+const { Title } = Typography;
 
 const BaseLayout = () => {
   return (
     <Layout className="layout">
-      <Header>AForm</Header>
+      <Header className="layout-header">
+        <Title keyboard level={1}>
+          AForm
+        </Title>
+      </Header>
       <Layout>
-        <Sider>Sider</Sider>
+        <Sider className="layout-sider">
+          <Menu>
+            <Menu.Item className="menu-router-item">
+              <Link to="/">disabled</Link>
+            </Menu.Item>
+          </Menu>
+        </Sider>
         <Content className="content">
           <Router>{RenderRoutes(routes)}</Router>
         </Content>
       </Layout>
-      <Footer>Footer</Footer>
     </Layout>
   );
 };
--- src/pages/form/base-form/index.tsx
+++ src/pages/form/base-form/index.tsx
@@ -1,29 +1,41 @@
 import React, { useEffect, useState } from 'react';
-import { Input, InputNumber, Button, AutoComplete } from 'antd';
 import AItem from '@/components/FormItem';
 import AForm from '@/components/Form';
-const { Option } = AutoComplete;
+import {
+  Form,
+  Select,
+  InputNumber,
+  Switch,
+  Radio,
+  Slider,
+  Button,
+  Upload,
+  Rate,
+  Checkbox,
+  Row,
+  Col,
+} from 'antd';
+import { UploadOutlined, InboxOutlined } from '@ant-design/icons';
+
+const { Option } = Select;
 const layout = {
   labelCol: { span: 4 },
   wrapperCol: { span: 16 },
 };
 
-/* eslint-disable no-template-curly-in-string */
-const validateMessages = {
-  required: '${label} is required!',
-  types: {
-    email: '${label} is not a valid email!',
-    number: '${label} is not a valid number!',
-  },
-  number: {
-    range: '${label} must be between ${min} and ${max}',
-  },
+const normFile = (e: any) => {
+  console.log('Upload event:', e);
+  if (Array.isArray(e)) {
+    return e;
+  }
+  return e && e.fileList;
 };
-/* eslint-enable no-template-curly-in-string */
 
 const BaseForm = () => {
-  const [result, setResult] = useState<string[]>([]);
   const [disabled, setDisabled] = useState(false);
+  const onFinish = (values: any) => {
+    console.log('Received values of form: ', values);
+  };
   return (
     <React.Fragment>
       <Button
@@ -33,36 +45,150 @@ const BaseForm = () => {
         设置disabled： {`${disabled}`}
       </Button>
       <AForm
+        name="validate_other"
         {...layout}
+        onFinish={onFinish}
         disabled={disabled}
-        name="nest-messages"
-        validateMessages={validateMessages}>
-        <AItem name={['user', 'name']} label="Name">
-          <Input />
-          <Input />
-          <Input />
+        initialValues={{
+          'input-number': 3,
+          'checkbox-group': ['A', 'B'],
+          rate: 3.5,
+        }}>
+        <AItem label="Plain Text">
+          <span className="ant-form-text">China</span>
         </AItem>
-        <AItem name={['user', 'email']} label="Email" rules={[{ type: 'email' }]}>
-          <AutoComplete style={{ width: 200 }} placeholder="input here">
-            {result.map((email: string) => (
-              <Option key={email} value={email}>
-                {email}
-              </Option>
-            ))}
-          </AutoComplete>
+        <AItem
+          name="select"
+          label="Select"
+          hasFeedback
+          rules={[{ required: true, message: 'Please select your country!' }]}>
+          <Select placeholder="Please select a country">
+            <Option value="china">China</Option>
+            <Option value="usa">U.S.A</Option>
+          </Select>
         </AItem>
-        <AItem name={['user', 'age']} label="Age" rules={[{ type: 'number', min: 0, max: 99 }]}>
-          <InputNumber />
+
+        <AItem
+          name="select-multiple"
+          label="Select[multiple]"
+          rules={[
+            { required: true, message: 'Please select your favourite colors!', type: 'array' },
+          ]}>
+          <Select mode="multiple" placeholder="Please select favourite colors">
+            <Option value="red">Red</Option>
+            <Option value="green">Green</Option>
+            <Option value="blue">Blue</Option>
+          </Select>
         </AItem>
-        <AItem label="Website">
-          <AItem name={['user', 'website']} label="Website">
-            <Input />
+
+        <AItem label="InputNumber">
+          <AItem name="input-number" noStyle>
+            <InputNumber min={1} max={10} />
+          </AItem>
+          <span className="ant-form-text"> machines</span>
+        </AItem>
+
+        <AItem name="switch" label="Switch" valuePropName="checked">
+          <Switch />
+        </AItem>
+
+        <AItem name="slider" label="Slider">
+          <Slider
+            marks={{
+              0: 'A',
+              20: 'B',
+              40: 'C',
+              60: 'D',
+              80: 'E',
+              100: 'F',
+            }}
+          />
+        </AItem>
+
+        <AItem name="radio-group" label="Radio.Group">
+          <Radio.Group>
+            <Radio value="a">item 1</Radio>
+            <Radio value="b">item 2</Radio>
+            <Radio value="c">item 3</Radio>
+          </Radio.Group>
+        </AItem>
+
+        <AItem
+          name="radio-button"
+          label="Radio.Button"
+          rules={[{ required: true, message: 'Please pick an item!' }]}>
+          <Radio.Group>
+            <Radio.Button value="a">item 1</Radio.Button>
+            <Radio.Button value="b">item 2</Radio.Button>
+            <Radio.Button value="c">item 3</Radio.Button>
+          </Radio.Group>
+        </AItem>
+
+        <AItem name="checkbox-group" label="Checkbox.Group">
+          <Checkbox.Group>
+            <Row>
+              <Col span={8}>
+                <Checkbox value="A" style={{ lineHeight: '32px' }}>
+                  A
+                </Checkbox>
+              </Col>
+              <Col span={8}>
+                <Checkbox value="B" style={{ lineHeight: '32px' }} disabled>
+                  B
+                </Checkbox>
+              </Col>
+              <Col span={8}>
+                <Checkbox value="C" style={{ lineHeight: '32px' }}>
+                  C
+                </Checkbox>
+              </Col>
+              <Col span={8}>
+                <Checkbox value="D" style={{ lineHeight: '32px' }}>
+                  D
+                </Checkbox>
+              </Col>
+              <Col span={8}>
+                <Checkbox value="E" style={{ lineHeight: '32px' }}>
+                  E
+                </Checkbox>
+              </Col>
+              <Col span={8}>
+                <Checkbox value="F" style={{ lineHeight: '32px' }}>
+                  F
+                </Checkbox>
+              </Col>
+            </Row>
+          </Checkbox.Group>
+        </AItem>
+
+        <AItem name="rate" label="Rate">
+          <Rate />
+        </AItem>
+
+        <AItem
+          name="upload"
+          label="Upload"
+          valuePropName="fileList"
+          getValueFromEvent={normFile}
+          extra="longgggggggggggggggggggggggggggggggggg">
+          <Upload name="logo" action="/upload.do" listType="picture">
+            <Button icon={<UploadOutlined />}>Click to upload</Button>
+          </Upload>
+        </AItem>
+
+        <AItem label="Dragger">
+          <AItem name="dragger" valuePropName="fileList" getValueFromEvent={normFile} noStyle>
+            <Upload.Dragger name="files" action="/upload.do">
+              <p className="ant-upload-drag-icon">
+                <InboxOutlined />
+              </p>
+              <p className="ant-upload-text">Click or drag file to this area to upload</p>
+              <p className="ant-upload-hint">Support for a single or bulk upload.</p>
+            </Upload.Dragger>
           </AItem>
         </AItem>
-        <AItem name={['user', 'introduction']} label="Introduction">
-          <Input.TextArea />
-        </AItem>
-        <AItem wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+
+        <AItem wrapperCol={{ span: 12, offset: 6 }}>
           <Button type="primary" htmlType="submit">
             Submit
           </Button>
